5813dc87f3c2268660daf0cc9c8eaeafecbeeaa7
DineshMKJuniorDevloper <150262225+DineshMKJuniorDevloper@users.noreply.github.com>
2023-11-10 02:43:22 -0800

    Add files via upload

---
+++ EmailSenderService.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail,
+                          String subject,
+                          String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+         message.setTo(toEmail);
+         message.setText(body);
+         message.setSubject(subject);
+         mailSender.send(message);
+          System.out.println("Message sent successfully....");
+    }
+}
